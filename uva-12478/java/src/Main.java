4651eedd556a1b1ea4e3e06975a0b074dbccf1ec
Henry Xiang <henryxiang@gmail.com>
2016-09-09 22:01:32 -0700

    updated 12478 java impl

--- uva-12478/java/src/Main.java
+++ uva-12478/java/src/Main.java
@@ -4,67 +4,71 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
-    
-    private static Map<String, Integer> names = new HashMap<>();
-    private static List<String> grid = new ArrayList<>();
-    
+    private static String[] names = {
+        "RAKIBUL",
+        "ANINDYA",
+        "MOSHIUR",
+        "SHIPLU",
+        "KABIR",
+        "SUNNY",
+        "OBAIDA",
+        "WASI"
+    };
+    private static String[] grid = {
+        "OBIDAIBKR",
+        "RKAULHISP",
+        "SADIYANNO",
+        "HEISAWHIA",
+        "IRAKIBULS",
+        "MFBINTRNO",
+        "UTOYZIFAH",
+        "LEBSYNUNE",
+        "EMOTIONAL"
+    };
+    private static Map<String, Integer> namesMap = null;
+    private static List<String> gridList = null;
+
     public static void main(String[] args) {
         readInData();
         solve();
-    } 
-    
-    private static void readInData() {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            String line = sc.nextLine();
-            if (line.equals("--name--")) {
-                for (int i = 0; i < 9; i++) {
-                    line = sc.nextLine();
-//                    System.out.println(line);
-                    names.put(line, 0);
-                }
-            }
-            else if (line.equals("--grid--")) {
-                for (int i = 0; i < 9; i++) {
-                    line = sc.nextLine();
-//                    System.out.println(line);
-                    grid.add(line);
-                }
-                for (int col = 0; col < 9; col++) {
-                    StringBuilder sb = new StringBuilder();
-                    for (int row = 0; row < 9; row++) {
-                        sb.append(grid.get(row).substring(col, col+1));
-                    }
-                    grid.add(sb.toString());
-                }
-            }
-            else if (line.equals("--end--")) {
-                return;
-            }
-        } 
     }
-    
+
+    private static void readInData() {
+        namesMap = new HashMap<String, Integer>();
+        for (int i = 0; i < names.length; i++) {
+            namesMap.put(names[i], 0);
+        }
+        
+        gridList = new ArrayList<String>(Arrays.asList(grid));
+        for (int col = 0; col < grid[0].length(); col++) {
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < grid.length; row++) {
+                sb.append(gridList.get(row).substring(col, col + 1));
+            }
+            gridList.add(sb.toString());
+        }
+    }
+
     private static void solve() {
-        for (String name : names.keySet()) {
+        for (String name : namesMap.keySet()) {
             String n = sortString(name);
-            for (String chars : grid) {
+            for (String chars : gridList) {
                 for (int i = 0; i <= chars.length() - n.length(); i++) {
-                    if (n.equals(sortString(chars.substring(i, i+n.length())))) {
+                    if (n.equals(sortString(chars.substring(i, i + n.length())))) {
                         System.out.println(name + " is found in [" + chars + "]");
-                        names.put(name, names.get(name) + 1);
-                        if (names.get(name) >= 2) {
+                        namesMap.put(name, namesMap.get(name) + 1);
+                        if (namesMap.get(name) >= 2) {
                             System.out.println(name);
                             return;
                         }
                     }
                 }
-            }         
+            }
         }
     }
-    
+
     private static String sortString(String s) {
         char[] chars = s.toCharArray();
         Arrays.sort(chars);

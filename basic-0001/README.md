0ced15b089410836c89fb0ca5e2201a7afc55687
Henry Xiang <henryxiang@gmail.com>
2016-09-11 18:02:27 -0700

    created basic-0001 problem

---
+++ basic-0001/README.md
@@ -0,0 +1,33 @@
+## Under Attack
+
+### Introduction
+
+Imagine you will design a computer chess game and need to find out all the spots that are under attack by a piece given its current position.
+
+For this problem, we only consider three major pieces: queen(Q), rook(R), and kight(K).
+
+The chess board will be represented with a 8x8 2-D array of characters, which contains the following information: 
+
+1. If a piece is placed on a spot, that array element will be marked with the name of the piece ('Q', 'R', or 'K').
+2. If a spot is under attack, that array element will be marked with a '*' character.
+3. Otherwise, all array elements will be marked with a '_' character.
+
+You will be given the position of a piece under consideration and asked for printing chess board in ASCII format that displays the status of every spot on the board.
+
+### Sample Input
+``
+K 0 2
+```
+
+### Sample Output
+```
+* * K * * * * *
+_ _ * _ _ _ _ _
+_ _ * _ _ _ _ _
+_ _ * _ _ _ _ _
+_ _ * _ _ _ _ _
+_ _ * _ _ _ _ _
+_ _ * _ _ _ _ _
+_ _ * _ _ _ _ _
+
+```
--- uva-00100/java/src/Main.java
+++ uva-00100/java/src/Main.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
-
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    static {
+        logger.setLevel(Level.OFF);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             long a = 0, b = 0;
             a = scanner.nextLong();
             b = scanner.nextLong();
+            logger.log(Level.INFO, String.format("a = %d, b = %d", a, b));
+
             int max = 0;
             long start = Math.min(a, b);
             long end = Math.max(a, b);
@@ -23,6 +30,7 @@ public class Main {
 
     private static int run3nPlus1(long i) {
         int n = 1;
+        logger.log(Level.INFO, String.format("i = %d", i));
         while (i != 1) {
             n += 1;
             if (i % 2 == 1) {
@@ -30,6 +38,8 @@ public class Main {
             } else {
                 i = i / 2;
             }
+            logger.log(Level.INFO, String.format("  i = %d", i));
+
         }
         return n;
     }
